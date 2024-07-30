fcf8dd33e2ff9daf5f40ea02323200ac1d51d014
Viacheslav Vasilyev <avoidik@gmail.com>
2024-07-30 20:44:05 +0300

    add libepoxy with angle patch

---
+++ Formula/libepoxy-egl.rb
@@ -0,0 +1,79 @@
+class LibepoxyEgl < Formula
+  desc "Library for handling OpenGL function pointer management (with ANGLE patch applied)"
+  homepage "https://github.com/anholt/libepoxy"
+  url "https://download.gnome.org/sources/libepoxy/1.5/libepoxy-1.5.10.tar.xz"
+  sha256 "072cda4b59dd098bba8c2363a6247299db1fa89411dc221c8b81b8ee8192e623"
+  license "MIT"
+
+  # We use a common regex because libepoxy doesn't use GNOME's "even-numbered
+  # minor is stable" version scheme.
+  livecheck do
+    url :stable
+    regex(/libepoxy[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "meson" => :build
+  depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+
+  on_linux do
+    depends_on "freeglut"
+  end
+
+  {
+    "https://patch-diff.githubusercontent.com/raw/anholt/libepoxy/pull/239.diff" => "d687d04d7fa63e1b6a3e4e4c8fe0de1dcfc4574aeff4f13b3a79ee23129c6ad4",
+  }.each do |url, sha|
+    patch :p0 do
+      url url
+      sha256 sha
+    end
+  end
+
+  conflicts_with cask: [
+    "libepoxy",
+  ]
+
+  def install
+    system "meson", "setup", "build", *std_meson_args
+    system "meson", "compile", "-C", "build", "--verbose"
+    system "meson", "install", "-C", "build"
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+
+      #include <epoxy/gl.h>
+      #ifdef OS_MAC
+      #include <OpenGL/CGLContext.h>
+      #include <OpenGL/CGLTypes.h>
+      #include <OpenGL/OpenGL.h>
+      #endif
+      int main()
+      {
+          #ifdef OS_MAC
+          CGLPixelFormatAttribute attribs[] = {0};
+          CGLPixelFormatObj pix;
+          int npix;
+          CGLContextObj ctx;
+
+          CGLChoosePixelFormat( attribs, &pix, &npix );
+          CGLCreateContext(pix, (void*)0, &ctx);
+          #endif
+
+          glClear(GL_COLOR_BUFFER_BIT);
+          #ifdef OS_MAC
+          CGLReleasePixelFormat(pix);
+          CGLReleaseContext(pix);
+          #endif
+          return 0;
+      }
+    EOS
+    args = %w[-lepoxy]
+    args += %w[-framework OpenGL -DOS_MAC] if OS.mac?
+    args += %w[-o test]
+    system ENV.cc, "test.c", "-L#{lib}", *args
+    system "ls", "-lh", "test"
+    system "file", "test"
+    system "./test"
+  end
+end
